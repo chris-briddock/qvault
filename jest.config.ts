46cabf4f512e2bea7bc294bf5304fcc5481a5abb
Chris Briddock <briddockchristopher@gmail.com>
2026-06-22 14:20:08 +0100

    fix: chnge env setup and config for jest.

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,7 @@ const createJestConfig = nextJest({ dir: "./" });
 const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "node",
+  setupFiles: ["<rootDir>/jest.env.setup.ts"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
---
+++ jest.env.setup.ts
@@ -0,0 +1,3 @@
+process.env.SKIP_ENV_VALIDATION = "1";
+process.env.SESSION_SECRET = "test-session-secret-that-is-at-least-32-chars!!";
+process.env.SERVER_SECRET = "test-server-secret-that-is-at-least-32-chars!!";
